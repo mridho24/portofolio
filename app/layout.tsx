6bb11ee6b35c00b3f5a996da3a74e17a22a748e2
Muhammad Ridho <m.ridho22@mhs.usk.ac.id>
2026-08-04 15:42:54 +0700

    style: update global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next"
 import { Poppins, Inter } from "next/font/google"
-import { ScrollProgressBar } from "@/components/ui/ScrollProgressBar"
 import { BackToTop } from "@/components/ui/BackToTop"
 import { AnimationsProvider } from "@/components/AnimationsProvider"
 import "./globals.css"
@@ -34,7 +33,6 @@ export default function RootLayout({
       className={`${poppins.variable} ${inter.variable}`}
     >
       <body className="min-h-screen flex flex-col antialiased">
-        <ScrollProgressBar />
         <AnimationsProvider>
           {children}
         </AnimationsProvider>
